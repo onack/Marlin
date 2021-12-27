36f28292e7c3b39e193b0ae218fabb71ed567b02
Onack <mariu.eg@gmail.com>
2021-12-27 14:27:10 +0100

    LIN_ADVANCE test

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2000,7 +2000,7 @@
 #define LIN_ADVANCE
 #if ENABLED(LIN_ADVANCE)
   //#define EXTRA_LIN_ADVANCE_K // Enable for second linear advance constants
-  #define LIN_ADVANCE_K 0.22    // Unit: mm compression per 1mm/s extruder speed
+  #define LIN_ADVANCE_K 1.4    // Unit: mm compression per 1mm/s extruder speed
   //#define LA_DEBUG            // If enabled, this will generate debug information output over USB.
   #ifdef S_CURVE_ACCELERATION
     #define EXPERIMENTAL_SCURVE // Enable this option to permit S-Curve Acceleration
